357c232ee4faee821974537f0cd1c0e9d4376fb3
camille-eloah <camelogithub@gmail.com>
2024-12-17 19:43:03 -0300

    Sintese

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua_chave_secreta_aqui'
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'app.db')}"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
